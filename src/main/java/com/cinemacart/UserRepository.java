7fba21eefe0d6777811541f37eb393bdccdc2909
Winter <winter.tomas96@gmail.com>
2026-02-09 19:12:24 -0800

    Update UserRepository.java

--- src/main/java/com/cinemacart/UserRepository.java
+++ src/main/java/com/cinemacart/UserRepository.java
@@ -13,11 +13,32 @@ public class UserRepository {
 
     private final Firestore db;
 
-    public UserRepository(){
+    public UserRepository() {
         this.db = FirestoreClient.getFirestore();
     }
 
-}
+
 
 //Save user to Firestore database
+public void save(UserAccount account) {
+    try{
+      Map <String, Object> fireData = new HashMap<>();
+      fireData.put("userId", account.getUserId());  
+      fireData.put("username", account.getUsername());
+      fireData.put("email", account.getEmail());
+      fireData.put("passwordHash", account.getPasswordH());
 
+      db.collection("users").document(account.getUsername()).set(fireData).get();
+
+    } catch (Exception e){ 
+        throw new RuntimeException("Run time error occurred", e);
+    }
+}
+
+
+public boolean exists(String username) {
+    try {
+
+    }
+}
+}
