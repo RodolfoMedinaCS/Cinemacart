de3755d51f2a3c4e5142d25672e97975a93ff915
Winter <winter.tomas96@gmail.com>
2026-02-11 21:39:05 -0800

    Added existence method to check if user exists in database

--- src/main/java/com/cinemacart/UserRepository.java
+++ src/main/java/com/cinemacart/UserRepository.java
@@ -3,6 +3,9 @@ package com.cinemacart;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.management.RuntimeErrorException;
+
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.cloud.FirestoreClient;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
@@ -17,8 +20,6 @@ public class UserRepository {
         this.db = FirestoreClient.getFirestore();
     }
 
-
-
 //Save user to Firestore database
 public void save(UserAccount account) {
     try{
@@ -30,7 +31,7 @@ public void save(UserAccount account) {
 
       db.collection("users").document(account.getUsername()).set(fireData).get();
 
-    } catch (Exception e){ 
+    } catch (Exception e) { 
         throw new RuntimeException("Run time error occurred", e);
     }
 }
@@ -38,7 +39,19 @@ public void save(UserAccount account) {
 
 public boolean exists(String username) {
     try {
+        DocumentSnapshot snapshot = db.collection("users").document(username).get().get(); //Get the document snapshot for the specified username
+        return snapshot.exists(); //Return true if username/document in database exists, otherwise return false
 
+    } catch (Exception e) {
+        throw new RuntimeException("Error retrieving user", e);
     }
 }
+
+public UserAccount findByUsername(String username) {
+    try {
+        DocumentSnapshot snapshot = db.collection("users").document(username).get().get();
+
+        if(!snapshot.exists()){
+            return null; //Return null if user with specified username does not exist in database
+    }
 }
