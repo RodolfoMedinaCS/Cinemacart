b0f680925d74b653431fe4d2e97eb1a645e52f41
Saeed Sekandari <sekandari2003@gmail.com>
2026-02-18 14:26:07 -0800

    Update MovieRepository

--- src/main/java/com/cinemacart/MovieRepository.java
+++ src/main/java/com/cinemacart/MovieRepository.java
@@ -1,27 +1,28 @@
 package com.cinemacart;
-import java.util.ArrayList;
 
-//This class store all the available moves
+import java.util.List;
+
+// This class stores all the available movies
 public class MovieRepository {
-public static ArrayList<Movie> getAllMovies(){
-    ArrayList<Movie> movies = new ArrayList<>();
 
-    movies.add(new Movie(1,"Titanic","Drama",195,8.0));
-    movies.add(new Movie(2,"The Godfather I","Crime",175,9.2));
-    movies.add((new Movie(3,"The Godfather II","Crime",202,9.0)));
-    movies.add(new Movie(4,"Gladiator","Action",155,8.5));
-    movies.add(new Movie(5,"3 Idiots","Comedy",171,8.4));
-    movies.add(new Movie(6,"The Batman","Action",176, 7.8));
-    movies.add(new Movie(7," The Lion King", "Animation", 88,8.5));
-    movies.add(new Movie(8, "Fury", "War", 134,7.6));
-    movies.add(new Movie(9,"The Kite Runner", "Drama", 128, 7.6));
-    movies.add(new Movie(10, "Avatar:Fire and Ash", "Adventure",197, 7.4));
-    movies.add(new Movie(11, "The Dark Knight", "Action", 152, 9.0));
-    movies.add(new Movie(12,"Beauty and the Beast", "Romance", 129, 7.1));
+    // Return the list of available movies
+    public static List<Movie> getAllMovies() {
 
-    return movies;
+        return List.of(
+                new Movie(1, "Titanic", "Drama", 195, 8.0),
+                new Movie(2, "The Godfather I", "Crime", 175, 9.2),
+                new Movie(3, "The Godfather II", "Crime", 202, 9.0),
+                new Movie(4, "Gladiator", "Action", 155, 8.5),
+                new Movie(5, " 3 Idiots", "Comedy", 171, 8.4),
+                new Movie(6, "The Batman", "Action ", 176, 7.8),
+                new Movie(7, "The Lion King", "Animation", 88, 8.5),
+                new Movie(8, "Fury", "War", 134, 7.6),
+                new Movie(9, "The Kite Runner", "Drama", 128, 7.6),
+                new Movie(10, "Avatar: Fire and Ash ", " Adventure", 197, 7.4),
+                new Movie(11, "The Dark Knight", "Action", 152, 9.0),
+                new Movie(12, "Beauty and the Beast ", "Romance", 129, 7.1)
+        );
 
 
-
-}
+    }
 }
