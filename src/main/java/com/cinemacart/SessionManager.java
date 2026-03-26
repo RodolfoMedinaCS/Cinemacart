7c867b093d08890394394602ae3bb9ca13c26e5b
Winter <winter.tomas96@gmail.com>
2026-03-26 11:17:55 -0700

    Created SessionManager.java to generate session tokens for users upon logging in so they can utilize account features

---
+++ src/main/java/com/cinemacart/SessionManager.java
@@ -0,0 +1,44 @@
+package com.cinemacart;
+
+import java.util.HashMap:
+import java.util.Map;
+import java.util.UUID;
+
+/*  The primary method of this class is to manage sessions for users when they login.
+    Sessions are what allow a user to access account features such as booking, viewing booking history, and managing their account details. 
+    Without a session, logging in becomes meaningless, as the user would not be able to access any of the features that require authentication. 
+    Session tokens are stored as a map of sessionToken to email, allowing for easy retrieval of the user's email when they make requests that require authentication.
+    Session tokens are issued upon logging in and expires when the user logs out.
+*/    
+
+public class SessionManager {
+
+    private final Map<String, String> sessions = new HashMap<>();
+
+    // Generate a session token for a user upon login
+    public String generateSessionToken(String email) {
+        String sessionToken = UUID.randomUUID().toString();
+        sessions.put(sessionToken, email);
+        return token;
+    }
+
+    // Returns email associated with the session token, or null if the token is invalid
+    public String getEmailByToken(String sessionToken) {
+        if (sessionToken == null) {
+            return null;
+        }
+        return sessions.get(sessionToken);
+    }
+
+    // Returns true if the token exists in the sessions map, indicating that the session is valid
+    public boolean isValidSession(String sessionToken) {
+        return token != null && sessions.containsKey(sessionToken);
+    }
+
+    // Revoke session token when user logs out
+    public void invalidSsesion(String sessionToken) {
+        if (token != null) {
+            sessions.remove(sessionToken);
+        }
+    }
+}
